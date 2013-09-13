cd45920f669e828eab210d6f8b29e53efaa427b2
sano <casio.rz1@gmail.com>
2013-09-13 17:57:43 +0900

    エミュレータ起動可能にした

--- src/testApp.mm
+++ src/testApp.mm
@@ -19,26 +19,14 @@ void testApp::setup() {
 		int w = grabber.getWidth();
 		int h = grabber.getHeight();
 
-        colorCvVertical.allocate(w,h);
-        colorCvHorizontal.allocate(h,w);
-
-        colorCvSmallVertical.allocate(w/4, h/4);
-		colorCvSmallHorizontal.allocate(h/4, w/4);
-
-		grayCvVertical.allocate(w/4, h/4);
-		grayCvHorizontal.allocate(h/4, w/4);
-
-        colorCv=&colorCvVertical;
-        colorCvSmall=&colorCvSmallVertical;
-        grayCv=&grayCvVertical;
-
-        //検分器の設定
-		finder.setNeighbors(1);
-		finder.setScaleHaar(1.5);
 	#else
         //カメラ使わない場合は画像読み込み
-        img.loadImage("test.jpg");
+        img.loadImage("sample.png");
     #endif
+
+    //検分器の設定
+    finder.setNeighbors(1);
+    finder.setScaleHaar(1.5);
 
     camera = new ofxiOSImagePicker();
     camera->setMaxDimension(480);
@@ -71,22 +59,24 @@ void testApp::update() {
         *grayCv = tempCv;
 		finder.findHaarObjects(*grayCv);
 		faces = finder.blobs;
-
+        //検出した顔情報の配列
+        faces = finder.blobs;
 	#else
         //we don't really need to do this every frame
         //but it simulates closer what the camera demo would be doing
-        finder.findHaarObjects(img);
+        //finder.findHaarObjects(img);
     #endif
 
-    //検出した顔情報の配列
-    faces = finder.blobs;
 }
 
 //--------------------------------------------------------------
 void testApp::draw() {
-
     ofSetColor(255);
-    drawCamView();
+    #ifdef USE_CAMERA
+        drawCamView();
+    #else
+        img.draw(0, 0);
+    #endif
     ofEnableAlphaBlending();
     ofSetColor(230, 0, 255, 200);
     ofRect(0, 0, ofGetWidth(), 16);
@@ -95,39 +85,37 @@ void testApp::draw() {
     gui.draw();
 }
 
+
 void testApp::drawCamView(){
-    float scaleFactor = 1.0;
+#ifdef USE_CAMERA
+     float scaleFactor = 1.0;
+     grabber.draw(0, 0);
+     scaleFactor = 4.0;
+     ofxCvColorImage img;
+     img.allocate(grabber.width, grabber.height);
+     img = grabber.getPixels();
+     unsigned char *pixels = img.getPixels();
 
-    #ifdef USE_CAMERA
-        grabber.draw(0, 0);
-        scaleFactor = 4.0;
-        ofxCvColorImage img;
-        img.allocate(grabber.width, grabber.height);
-        img = grabber.getPixels();
-        unsigned char *pixels = img.getPixels();
+     int w = img.width;
+     int h = img.height;
 
-        int w = img.width;
-        int h = img.height;
-
-        //set mosaic
-        int skip = size;
-        for (int k = 0; k < faces.size(); k++) {
-            ofRectangle rect(faces[k].boundingRect.x * scaleFactor, faces[k].boundingRect.y * scaleFactor, faces[k].boundingRect.width * scaleFactor, faces[k].boundingRect.width * scaleFactor);
-            for (int j = rect.y; j < rect.y + rect.height; j += skip) {
-                for (int i = rect.x; i < rect.x + rect.width; i += skip) {
-                    int valueR = pixels[j * w + i];
-                    int valueG = pixels[j * w + i + 1];
-                    int valueB = pixels[j * w + i + 2];
-                    ofSetColor(valueR, valueG, valueB);
-                    ofRect(i, j, skip, skip);
-                }
-            }
-        }
-
-    #else
-        img.draw(0, 0);
-    #endif
+     //set mosaic
+     int skip = size;
+     for (int k = 0; k < faces.size(); k++) {
+         ofRectangle rect(faces[k].boundingRect.x * scaleFactor, faces[k].boundingRect.y * scaleFactor, faces[k].boundingRect.width * scaleFactor, faces[k].boundingRect.width * scaleFactor);
+         for (int j = rect.y; j < rect.y + rect.height; j += skip) {
+             for (int i = rect.x; i < rect.x + rect.width; i += skip) {
+                int valueR = pixels[j * w + i];
+                int valueG = pixels[j * w + i + 1];
+                int valueB = pixels[j * w + i + 2];
+                ofSetColor(valueR, valueG, valueB);
+                ofRect(i, j, skip, skip);
+             }
+         }
+     }
+     #endif
 }
+
 //--------------------------------------------------------------
 void testApp::exit() {
 
